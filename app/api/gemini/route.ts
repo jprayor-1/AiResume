eee7832f81721aa19e22f1fe350cb6f441dfcb28
Jesaiah Prayor <jesaiahprayor@jesaiahs-mbp.mynetworksettings.com>
2025-11-13 20:35:22 -0500

    Update Gemini to parse documents

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -1,5 +1,10 @@
 import { NextResponse } from 'next/server';
 import { GoogleGenerativeAI } from '@google/generative-ai';
+import { readFile, writeFile } from 'node:fs/promises';
+import fs from 'fs';
+import path from 'path';
+// @ts-expect-error: pdf-parse CJS import
+import pdfParse from 'pdf-parse/lib/pdf-parse.js';
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || '');
 
@@ -7,8 +12,15 @@ export async function POST(req: Request) {
   try {
     const { prompt } = await req.json(); // how does this work
 
+    const filePath = path.join(process.cwd(), 'JprayorResume.pdf');
+    const buffer = fs.readFileSync(filePath);
+
+    // pdf-parse v1.x
+    const pdfData = await pdfParse(buffer);
+    const resumeText = pdfData.text;
+
     const model = genAI.getGenerativeModel({ model: 'gemini-2.5-flash' });
-    const result = await model.generateContent(prompt);
+    const result = await model.generateContent(`can you summarize my resume: /n${resumeText}`);
     const text = result.response.text();
 
     return NextResponse.json({ output: text });
